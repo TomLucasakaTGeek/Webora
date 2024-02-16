d0d840369cf458ab6a51190a15efcb383abf1835
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2024-02-16 21:31:27 +0530

    Completing Netflix Clone

---
+++ Portal/Backend/index.js
@@ -0,0 +1,17 @@
+const express=require("express")();
+const zod=require("zod")();
+
+/*
+1)secure payment gateway
+2)create profiles and track donations
+3)recieve updates on campaigns
+4)Implement a referral system to incentivize users to share campaigns their networks
+5)Display clear visuals and data on campaign progress and impact
+6)Ensure the website is responsive for all devices
+*/
+
+obj={
+    Name: "prashant shukla",
+    Rf-code:"pft3702",
+    Donations: 20000
+}
